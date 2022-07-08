eb587e1c14f5ed0d744a42170ee5d5ea4b487dd2
Eduard Tokarev <etdevios@gmail.com>
2022-07-09 00:50:54 +0300

    84. Conditional Statements Challenge Solution

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+//    let softTime = 5
+//    let mediumTime = 7
+//    let hardTime = 12
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
 
+        let hardness = sender.currentTitle
+        
+        if hardness == "Soft" {
+            print(eggTimes["Soft"]!)
+        } else if hardness == "Medium" {
+             print(eggTimes["Medium"]!)
+        } else {
+            print(eggTimes["Hard"]!)
+        }
+        
+    }
+    
 
 }
