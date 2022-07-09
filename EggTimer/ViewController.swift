cfc772225648d3e4a0855962f37f35cdab11dd49
Eduard Tokarev <etdevios@gmail.com>
2022-07-09 18:02:38 +0300

    Completed

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,27 +7,69 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
-    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
-//    let softTime = 5
-//    let mediumTime = 7
-//    let hardTime = 12
+    let eggTimes = ["Soft": 300 , "Medium": 420, "Hard": 720]
+    var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
+    var player: AVAudioPlayer?
+    var timerBar = Timer()
     
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var labelLike: UILabel!
     @IBAction func hardnessSelected(_ sender: UIButton) {
 
-        let hardness = sender.currentTitle
+        timer.invalidate()
+        let hardness = sender.currentTitle!
+        totalTime = eggTimes[hardness]!
+        progressBar.progress = 0
+        secondsPassed = 0
+        labelLike.text = hardness
+        timeLabel.text = "\(String(format: "%02d", (totalTime / 60))) : \(String(format: "%02d", (totalTime % 60)))"
         
-        if hardness == "Soft" {
-            print(eggTimes["Soft"]!)
-        } else if hardness == "Medium" {
-             print(eggTimes["Medium"]!)
-        } else {
-            print(eggTimes["Hard"]!)
-        }
-        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+
     }
     
+    @objc func updateCounter() {
+        
+        if secondsPassed < totalTime {
+
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
+            secondsPassed += 1
+            
+            timeLabel.text = "\(String(format: "%02d", (totalTime - secondsPassed) / 60)) : \(String(format: "%02d", (totalTime - secondsPassed) % 60))"
+        } else {
+            timer.invalidate()
+            labelLike.text = "DONE!"
+            progressBar.progress = 1
+            playSound()
+        }
+    }
+    
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3") else
+        { return }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            
+            guard let player = player else {
+                return
+            }
+            
+            player.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 
 }
